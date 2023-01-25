6cc739e16003cda85e5c4dc1d2339f83a159460c
Irene Yeh <pigrabbit87@yahoo.com.tw>
2023-01-24 19:36:47 -0800

    fix manual

--- scraper.py
+++ scraper.py
@@ -1,42 +1,45 @@
 import requests, bs4, json, time
 
 VILLAGERS = [
-    # 'Alex',
-    # 'Elliot',
-    # 'Harvey',
-    # 'Sam',
-    # 'Sebastian',
-    # 'Shane',
-    # 'Abigail',
-    # 'Emily',
-    # 'Haley',
-    # 'Leah',
-    # 'Maru',
-    # 'Penny',
-    # 'Caroline',
-    # 'Clint',
-    # 'Demetrius',
-    # 'Dwarf',
-    # 'Evelyn',
-    # 'George',
-    # 'Gus',
-    # 'Jas',
-    # 'Jodi',
-    # 'Kent',
-    # 'Krobus',
-    # 'Leo',
-    # 'Lewis',
-    # 'Linus',
-    # 'Marnie',
-    # 'Pam',
-    # 'Pierre',
-    # 'Robin',
+    'Alex',
+    'Elliot',
+    'Harvey',
+    'Sam',
+    'Sebastian',
+    'Shane',
+    'Abigail',
+    'Emily',
+    'Haley',
+    'Leah',
+    'Maru',
+    'Penny',
+    'Caroline',
+    'Clint',
+    'Demetrius',
+    'Dwarf',
+    'Evelyn',
+    'George',
+    'Gus',
+    'Jas',
+    'Jodi',
+    'Kent',
+    'Krobus',
+    'Leo',
+    'Lewis',
+    'Linus',
+    'Marnie',
+    'Pam',
+    'Pierre',
+    'Robin',
     'Sandy',
     'Vincent',
     'Willy',
     'Wizard',
 ]
 
+DATES = ['1st', '2nd', '3rd', *[f'{num}th' for num in range(4, 29)]]
+
+# Process from raw html data
 def process_person(name):
     # Scrape or load
     raw_data = load_raw_villager_data(name)
@@ -49,8 +52,30 @@ def process_person(name):
     person_data['basic'] = parse_info(soup)
 
     # Parse schedule
-    if name not in {"Wizard", "Sandy"}:
+    if name not in {"Wizard", "Sandy", "Dwarf"}:
         person_data['schedule'] = process_schedules(soup)
+
+    with open(f'villagers/parsed/{name}.json', 'w') as f:
+            json.dump(person_data, f, indent=2)
+
+
+# Improve from parsed json
+def improve_person(name):
+    with open(f'villagers/parsed/{name}.json', 'r') as f:
+        person_data = json.load(f)
+
+    person_data = improve_schedule(person_data)
+
+    with open(f'villagers/parsed/{name}.json', 'w') as f:
+        json.dump(person_data, f, indent=2)
+
+
+def improve_schedule(person_data):
+    # Raining
+    schedules = person_data["schedule"]
+    for idx, schedule in enumerate(schedules):
+        if "raining" in schedule["name"].lower():
+            person_data["schedule"][idx]["has_rain"] = True
 
     return person_data
 
@@ -138,7 +163,7 @@ def process_nested_schedule_table(element):
                         "time": time,
                         "description": description
                     })
-            schedules.append({"name": schedule_name, "schedules": time_and_locations})
+            schedules.append({"name": schedule_name, **parse_schedule_name(schedule_name), "schedules": time_and_locations})
 
     return schedules
 
@@ -157,17 +182,71 @@ def process_schedule_table(element):
                 "description": description
             })
 
-    return {"name": schedule_name, "schedules": time_and_locations}
+    return {"name": schedule_name, **parse_schedule_name(schedule_name), "schedules": time_and_locations}
 
+
+def parse_schedule_name(schedule_name):
+    """
+    {
+        "season": "",
+        "DOW": [],
+        "has_rain":,
+        "date": []
+    }
+    """
+
+    schedule_name = schedule_name.lower().replace(',', '')
+    default = "regular schedule" in schedule_name
+
+    schedule_tokens = schedule_name.split(' ')
+    uncertain = "deviations" in schedule_tokens
+
+    heart_dependent = "heart" in schedule_tokens or "hearts" in schedule_tokens
+
+    heart_condition = {}
+    if "less than 6 hearts" in schedule_name or "not at 6 hearts" in schedule_name:
+        with_index = schedule_tokens.index('with')
+        heart_condition['less_than_six'] = " ".join(schedule_tokens[with_index+1:])
+    elif "6+ hearts" in schedule_name:
+        with_index = schedule_tokens.index('with')
+        heart_condition['more_than_six'] = " ".join(schedule_tokens[with_index+1:])
+
+    community_center_repaired_dependent = "community center repaired" in schedule_name
+
+    # Not doing marriage for now
+    if "marriage" in schedule_tokens:
+        return {}
+
+    try:
+        season = next(s for s in ['spring', 'summer', 'fall', 'winter'] if s in schedule_tokens)
+    except StopIteration:
+        season = None
+
+    dow = [d for d in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'] if d in schedule_tokens]
+
+    has_rain = "rain" in schedule_tokens or "rainy" in schedule_tokens or "raining" in schedule_tokens
+
+    date = [idx+1 for idx, date in enumerate(DATES) if date in schedule_tokens]
+
+    return {
+        "season": season,
+        "default": default,
+        "uncertain": uncertain,
+        "heart_dependent": heart_dependent,
+        "heart_condition": heart_condition,
+        "community_center_repaired_dependent": community_center_repaired_dependent,
+        "DOW": dow,
+        "has_rain": has_rain,
+        "date": date
+    }
 
 class UrlNotFound(Exception):
     pass
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     for person in VILLAGERS:
         print(f"Processing {person}.")
-        person_data = process_person(person)
+        improve_person(person)
 
-        with open(f'villagers/parsed/{person}.json', 'w') as f:
-            json.dump(person_data, f, indent=2)
+        
